b416fda7b0a3d0c345bd91b9225d184eba9998b5
Sharique Ahmed <shariquepervez2000@gmail.com>
2025-09-30 11:29:14 +0530

    added pubsub name

--- src/main/java/com/hdfcbank/neftiladmiproccessor/utils/Constants.java
+++ src/main/java/com/hdfcbank/neftiladmiproccessor/utils/Constants.java
@@ -3,7 +3,7 @@ package com.hdfcbank.neftiladmiproccessor.utils;
 public class Constants {
 
 
-    public static final String KAFKA_RESPONSE_TOPIC_DAPR_BINDING = "kafka-nilrouter-pubsub-component";
+    public static final String KAFKA_RESPONSE_TOPIC_DAPR_BINDING = "kafka-admi-processor-pubsub-component";
     public static final String FC_DISPATCHER = "FC_DISPATCHER";
     public static final String EPH_DISPATCHER = "EPH_DISPATCHER";
     public static final String SENT_TO_DISPATCHER = "SENT_TO_DISPATCHER";
